53459e25c55c411da1f5118f9dc3773bfc6aaab7
fatan <fatan.aminullah.j@gmail.com>
2021-10-16 11:36:18 +0700

    update context

--- src/components/loader.js
+++ src/components/loader.js
@@ -5,25 +5,25 @@ import Lottie from "react-lottie"
 
 export const Loader = ({ className }) => {
   const [loader, setLoader] = useState(false)
-  const { initialLoading, setInitialLoading } = useContext(LoadingContext)
+  const context = useContext(LoadingContext)
 
   useEffect(() => {
-    if (initialLoading) {
+    if (context?.initialLoading) {
       setLoader(true)
       setTimeout(() => {
-        setInitialLoading(prevInitialLoading => !prevInitialLoading)
+        context?.setInitialLoading(prevInitialLoading => !prevInitialLoading)
 
         setTimeout(() => {
           setLoader(false)
         }, 1000)
       }, 2500)
     }
-  }, [setInitialLoading])
+  }, [context?.setInitialLoading])
 
   return (
     <>
       {/* prettier-ignore */}
-      <div className={`loader loader-dark loader-lottie fadeOut ${className} ${!initialLoading ? `loader-exit ${!loader ? `d-none` : ``}` : ""}`}>
+      <div className={`loader loader-dark loader-lottie fadeOut ${className} ${!context?.initialLoading ? `loader-exit ${!loader ? `d-none` : ``}` : ""}`}>
         <div className="img-lottie">
           <Lottie
             options={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import * as React from "react"
+import React, { useContext, useEffect, useLayoutEffect } from "react"
 import Cover from "../components/cover"
 import { Loader } from "../components/loader"
 import SEO from "../components/seo"
 import { LoadingContext } from "../context/loading-context"
 
 const IndexPage = () => {
-  const { initialLoading } = React.useContext(LoadingContext)
-  React.useEffect(() => {
+  const context = useContext(LoadingContext)
+  useEffect(() => {
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty("--vh", `${vh}px`)
   }, [])
-  React.useLayoutEffect(() => {
+  useLayoutEffect(() => {
     const wrapper = document.querySelector("main")
     wrapper.classList.add("animated", "fadeIn")
   }, [])
@@ -18,7 +18,7 @@ const IndexPage = () => {
     <main>
       <Loader />
       <SEO />
-      {!initialLoading ? (
+      {!context?.initialLoading ? (
         <>
           <Cover />
         </>

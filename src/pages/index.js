250df9e29c3e85adddfdabf276fcf27ada3fccdc
fatan <fatan.aminullah.j@gmail.com>
2021-10-18 03:05:30 +0700

    create event location section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React, { useContext, useEffect, useLayoutEffect } from "react"
+import 'react-gallery-carousel/dist/index.css'
 import Bride from "../components/bride"
 import Countdown from "../components/countdown"
 import Cover from "../components/cover"
+import EventAndLocation from "../components/event-and-location"
 import Gallery from "../components/gallery"
 import { Loader } from "../components/loader"
 import Protocols from "../components/protocols"
 import SEO from "../components/seo"
 import Verse from "../components/verse"
 import { LoadingContext } from "../context/loading-context"
-import 'react-gallery-carousel/dist/index.css';
 
 const IndexPage = () => {
   const context = useContext(LoadingContext)
@@ -32,6 +33,7 @@ const IndexPage = () => {
             <Bride />
             <Countdown />
             <Gallery />
+            <EventAndLocation />
             <Protocols />
           </>
         ) : <div />}
